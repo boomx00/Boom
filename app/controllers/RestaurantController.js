2c3c99b20ae07375d1617bf373cd3eefb8c31e93
= <adrianskevin00@gmail.com>
2021-06-19 16:00:01 +0700

    manage restaurant

--- app/controllers/RestaurantController.js
+++ app/controllers/RestaurantController.js
@@ -5,6 +5,7 @@ class RestaurantController {
 
     async addRestaurant(req,res,next){
         try{
+            // Send register restaurant
             const {name,address,logo_url} = req.body
             await db.transaction(async(t)=>{
                 try{
@@ -30,7 +31,52 @@ class RestaurantController {
                   'err': err
                 })
     }
+    }
 
+    async manageRestaurant(req,res,next){
+        try{
+            // confirm restaurant
+            const {id, status} = req.body;
+            // if(status = "confirmed"){
+            //     await db.transaction(async(t)=>{
+            //     try{
+            //         const confirmRestaurant = await t('restaurants').where('id','=',id).update({
+            //             verified: 1,
+            //             status: "accepted" 
+            //         })
+            //         await t.commit();
+            //     }catch(err){
+            //         await t.rollback();
+            //         throw err;
+            //     }
+            // })
+            // res.status(201).send({
+            //     'status': 'RESTAURANT_VERIFIED',
+            //     'msg': 'Restaurant has been verified'
+            // })
+            // }
+
+            // if(status = "declined"){
+            //     await db.transaction(async(t)=>{
+            //         try{
+            //             const confirmRestaurant = await t('restaurants').where('id','=',id).update({
+            //                 verified: 1 
+            //             })
+            //             await t.commit();
+            //         }catch(err){
+            //             await t.rollback();
+            //             throw err;
+            //         }
+            //     })
+            //     res.status(201).send({
+            //         'status': 'RESTAURANT_VERIFIED',
+            //         'msg': 'Restaurant has been verified'
+            //     })
+            // }
+          
+        }catch(err){
+
+        }
     }
 }
 
--- app/routes/restaurantRoutes.js
+++ app/routes/restaurantRoutes.js
@@ -4,5 +4,7 @@ const tokenVerifier = require('../middleware/tokenVerifier');
 
 // create add restaurant
 app.post("/add-restaurant", restaurantController.addRestaurant);
+// manage restaurant requests
+app.put("/manage-restaurant",restaurantController.manageRestaurant);
 
 module.exports = app;
