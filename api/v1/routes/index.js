ffaecf418845f2c28eabe78d748c348314b78ccb
= <adrianskevin00@gmail.com>
2021-06-23 18:21:26 +0700

    added manage reservation

--- api/v1/routes/index.js
+++ api/v1/routes/index.js
@@ -1,9 +1,11 @@
 const app = require('express').Router();
 const userRoutes = require('./userRoutes')
 const restaurantRoutes = require('./restaurantRoutes')
+const reservationRoutes = require('./reservationRoutes')
 
 //  Routes
 app.use('/user', userRoutes);
 app.use('/restaurant', restaurantRoutes);
+app.use('/reservation', reservationRoutes);
 
 module.exports = app;
